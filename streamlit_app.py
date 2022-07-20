5774b8348236e9466446db1512948ec83580d859
unni2016 <unni2b4u@yahoo.com>
2022-07-20 18:41:15 +0530

    Update streamlit_app.py
    added streamline dataframe to the normalized json data

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,4 +32,4 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.text(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
